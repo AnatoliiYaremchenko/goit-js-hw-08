94ffb52f26501de577cc1af656c30b0885eed889
AnatoliiYaremchenko <97903677+AnatoliiYaremchenko@users.noreply.github.com>
2023-02-27 09:00:37 +0300

    test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,7 +4,6 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-
 // console.log(galleryItems);
 
 const galleryRef = document.querySelector('.gallery');
